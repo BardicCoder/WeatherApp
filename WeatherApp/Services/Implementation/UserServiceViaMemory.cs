2f019f6cb487fa69db1582fa6bdaf7ed193f2049
bardiccoder <j.s.moore82@hotmail.com>
2019-06-03 01:55:19 -0700

    JSM: Basic unit test

---
+++ WeatherApp/Services/Implementation/UserServiceViaMemory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherApp.Data;
+
+namespace WeatherApp.Services
+{
+    public class UserServiceViaMemory : IUserService
+    {
+        public List<string> zipCodeHistory { get; set; } = new List<string>();
+
+        public List<SearchHistory> GetUserSearchHistory(int memberId)
+        {
+            List<SearchHistory> history = new List<SearchHistory>();
+            foreach(string zip in zipCodeHistory)
+            {
+                history.Add(new SearchHistory() { MemberId = 1, ZipCode = zip });
+            }
+            return history;
+        }
+
+        public void SaveZipCodeToSearchHistory(int memberId, string zipCode)
+        {
+            zipCodeHistory.Add(zipCode);
+        }
+    }
+}
--- WeatherAppTests/UnitTest1.cs
+++ WeatherAppTests/UnitTest1.cs
@@ -1,4 +1,10 @@
+using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
+using ThirdPartyApiCaller.Models;
+using ThirdPartyApiCaller.Utility;
+using WeatherApp.Data;
+using WeatherApp.Services;
 
 namespace Tests
 {
@@ -12,7 +18,14 @@ namespace Tests
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var mock = new Mock<IUserService>();
+
+            mock.Setup(m => m.GetUserSearchHistory(1)).Returns(new List<SearchHistory>() { new SearchHistory() { MemberId = 1, ZipCode = "99123" } });
+
+            var result = mock.Object.GetUserSearchHistory(1);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("99123", result[0].ZipCode);
         }
     }
 }
